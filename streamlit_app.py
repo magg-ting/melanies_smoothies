998789f3b42a82693471d21e7d5592dc0286d738
Mei Ting Lui <34826140+magg-ting@users.noreply.github.com>
2025-01-01 19:38:49 +0000

    use pd to find search_on

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,6 +20,11 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 # st.dataframe(data=my_dataframe, use_container_width=True)
 # st.stop()
 
+# convert snowflake df to pandas df
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
+
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',
     my_dataframe,
@@ -32,9 +37,9 @@ if ingredients_list:
         ingredients_string += fruit_chosen + ' '
         # API call
         st.subheader(fruit_chosen + ' Nutrition Information')
-        search_stmt = f"""select search_on from smoothies.public.fruit_options where fruit_name = {fruit_chosen}"""
-        search_on = session.sql(search_stmt).collect()
-        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{fruit_chosen}")
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     #st.write(ingredients_string)
 
